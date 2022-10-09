d0775a849e4671f8ed26c0308beecf404bde2bd5
KPiatigorskii <k.piatigorskii@gmail.com>
2022-10-09 19:42:55 +0300

    add first get query

--- source/constants.ts
+++ source/constants.ts
@@ -1,7 +1,8 @@
-export const DB_CONNECTION_STRING: string = "server=.;Database=masa_school;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server}";
+export const DB_CONNECTION_STRING: string = "server=.;Database=ashan_store;Trusted_Connection=Yes;Driver={ODBC Driver 17 for SQL Server}";
 export const NON_EXISTENT_ID: number = -1;
 
 export class Queries {
 
+    public static ProductById: string = "SELECT * FROM product WHERE id = ?";
     public static SelectIdentity: string = "SELECT SCOPE_IDENTITY() AS id;";
 }
---
+++ source/services/product.service.ts
@@ -0,0 +1,84 @@
+import { product } from "../entities";
+import { SqlHelper } from "../helpers/sql.helper";
+import { ErrorService } from "./error.service";
+import { Queries } from "../constants"
+import { Status } from "../enums"
+
+interface IProductService {
+    getAllProductsByStoreId(id: number): Promise<any>;
+    getProductById(id: number): Promise<any>;
+    updateProductById(id: number): Promise<any>;
+    createProduct(): Promise<any>;
+    deleteProductById(): Promise<any>;
+}
+
+interface localProduct {
+    id: number;
+    inner_uuid: number;
+    name: string;
+    create_date: Date;
+    update_date: Date;
+    create_user_id: number;
+    update_user_id: number;
+    category_id: number;
+    status_id: Status;
+
+    // createUser: string;
+    // updateUser: string;
+    // createDate: Date;
+    // updateDate: Date;
+    // statusId: number;
+}
+
+
+export class ProductService implements IProductService{
+
+    constructor(
+        private errorService: ErrorService
+    ) { }
+
+    public getAllProductsByStoreId(id: number){
+        return new Promise<product>((resolve, reject) => {
+        })
+    }
+    getProductById(id: number): Promise<product>{
+        return new Promise<product>((resolve, reject) => {
+            SqlHelper.executeQuerySingleResult<localProduct>(this.errorService, Queries.ProductById, id) // , Status.Active)
+            .then((queryResult: localProduct) => {
+                resolve(this.parseLocalProduct(queryResult));
+            })
+            .catch((error) => {
+                reject(error)
+            })
+        })
+    }
+    updateProductById(id: number): Promise<any>{
+        return new Promise<any>((resolve, reject) => {
+
+        })
+    }
+    createProduct(): Promise<any>{
+        return new Promise<any>((resolve, reject) => {
+
+        })
+    }
+    deleteProductById(): Promise<any>{
+        return new Promise<any>((resolve, reject) => {
+
+        })
+    }
+
+    private parseLocalProduct(local: localProduct): product {
+        return {
+            id: local.id,
+            productName: local.name,
+            innerUuid: local.inner_uuid,
+            categoryId: local.category_id,
+            createDate: local.create_date,
+            createUserId: local.create_user_id, 
+            statusId: local.status_id,
+            updateDate: local.update_date,
+            updateUserId: local.update_user_id
+        };
+    }
+}
