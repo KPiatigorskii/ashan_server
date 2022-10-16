0d8cdebfc09454ea2552775416b5d68ff09dc128
KPiatigorskii <k.piatigorskii@gmail.com>
2022-10-16 11:34:41 +0300

    add all product sevices

--- source/routes/product.routes.ts
+++ source/routes/product.routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import controller from '../controllers/product.controller'
 const router = express.Router();
 
+router.get('/get-all', controller.getAllProducts);
 router.get('/get-from-store/:id', controller.getAllProductByStoreId); // Get a list of all products by store id
 router.get('/get/:id', controller.getProductById); // Get an product by id
 router.put('/update/:id', controller.updateProductById); // Update product info by id (including category)
